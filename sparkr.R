610781109866ce7f1cd2d8f16d57aa9077699a1a
zw43 <zw43@duke.edu>
2016-04-27 00:38:49 -0400

    task 2 completed

--- sparkr.R
+++ sparkr.R
@@ -17,17 +17,37 @@ sqlContext = sparkRSQL.init(sc)
 j = read.json(sqlContext, "hdfs://localhost:8020/data/reddit/large.json")
 
 res = select(j, j$subreddit, j$created_utc) 
-res = mutate(res, created = from_unixtime(res$created_utc)) %>%
-  mutate(., month=month(.$created), wday=date_format(.$created,"E")) 
-res1 = group_by(res, res$month, res$subreddit) %>% count()
+res_t = mutate(res, created = from_unixtime(res$created_utc)) %>%
+  mutate(., month=month(.$created), wday=date_format(.$created,"E"), rounded=round(.$created_utc/3600)*3600, hour=date_format(.$created, "H")) 
+res1 = group_by(res_t, res_t$month, res_t$subreddit) %>% count()
+
+res2=group_by(res_t, res_t$wday, res_t$hour)%>% count()
+res_mon=filter(res2, res2$wday=="Mon")%>%head(n=24)
+res_tue=filter(res2, res2$wday=="Tue")%>%head(n=24)
+res_wed=filter(res2, res2$wday=="Wed")%>%head(n=24)
+res_thu=filter(res2, res2$wday=="Thu")%>%head(n=24)
+res_fri=filter(res2, res2$wday=="Fri")%>%head(n=24)
+res_sat=filter(res2, res2$wday=="Sat")%>%head(n=24)
+res_sun=filter(res2, res2$wday=="Sun")%>%head(n=24)
+save(res_mon,res_tue,res_wed,res_thu, res_fri, res_sat, res_sun, file="task2.Rdata")
+plot(res_mon$count, x=res_mon$hour, main="monday posts", xlab="hours", type='l', col="blue")
+plot(res_tue$count, x=res_tue$hour, main="tuesday posts", xlab="hours", type='l', col="red")
+plot(res_wed$count, x=res_wed$hour, main="wednesday posts", xlab="hours", type='l', col="brown")
+plot(res_thu$count, x=res_thu$hour, main="thursday posts", xlab="hours", type='l', col="cyan")
+plot(res_fri$count, x=res_fri$hour, main="friday posts", xlab="hours", type='l', col="green")
+plot(res_sat$count, x=res_sat$hour, main="saturday posts", xlab="hours", type='l', col="magenta")
+plot(res_sun$count, x=res_sun$hour, main="sunday posts", xlab="hours", type='l', col="black")
+
+
+
 
 j_rank=rep(0, 150)
 f_rank=rep(NA,150)
 m_rank=rep(NA, 150)
-res_jan = filter(res1, res$month == 1) %>% arrange(., desc(.$count)) %>% head(n=150)
+res_jan = filter(res1, res1$month == 1) %>% arrange(., desc(.$count)) %>% head(n=150)
 
-res_feb = filter(res1, res$month == 2) %>% arrange(., desc(.$count)) %>% head(n=150)
-res_mar = filter(res1, res$month == 3) %>% arrange(., desc(.$count)) %>% head(n=150)
+res_feb = filter(res1, res1$month == 2) %>% arrange(., desc(.$count)) %>% head(n=150)
+res_mar = filter(res1, res1$month == 3) %>% arrange(., desc(.$count)) %>% head(n=150)
 
 
 
@@ -49,7 +69,8 @@ save(res_jan25,res_feb25,res_mar25, file="task1.Rdata")
 
 
 
-res2=select(res2, res2$subreddit, res2$created, res2$wday, res2$created_utc)
+
+
 #graph for all days from jan to march by hours
 
 #graph 7 graphs of each day
